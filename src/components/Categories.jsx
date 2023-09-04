151440fdc35d98ae3b4623f1355850be781a165d
avanti575 <partners.pr@yandex.ru>
2023-09-04 14:21:15 +0300

    add react project

---
+++ src/components/Categories.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианские",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li
+            key={i}
+            onClick={() => onClickCategory(i)}
+            className={activeIndex === i ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Categories;
